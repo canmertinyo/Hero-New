3480abdd513941a2a44d439a2b38af3e4b110e81
CANMERTINYO <38213551+canmertinyo@users.noreply.github.com>
2023-01-01 00:33:10 +0300

    ..

--- services/generate-random-code.ts
+++ services/generate-random-code.ts
@@ -4,23 +4,25 @@ import { promises as fsPromises } from 'fs';
 import * as path from 'path';
 export class RandomCouponCode {
 
-    async readFile() {
-        try {
-          // ✅ Read contents of directory
-          const dirContents = await fsPromises.readdir(__dirname);
-          console.log(dirContents);
-      
-          // ✅ Read contents of `another-file.ts` in the same directory
-          const fileContents = await fsPromises.readFile(
-            path.join(__dirname, '../json/coupon-codes.json'),
-            { encoding: 'utf-8' },
-          );
-          console.log(fileContents);
-        } catch (err) {
-          console.log('error is: ', err);
-        }
-      }
 
+  // bu kısımı tekrar elden geçir.
+    // async readFile() {
+    //     try {
+    //       const dirContents = await fsPromises.readdir(__dirname);
+    //       console.log(dirContents);
+      
+    //       const fileContents = await fsPromises.readFile(
+    //         path.join(__dirname, '../json/coupon-codes.json'),
+    //         { encoding: 'utf-8' },
+    //       );
+    //       console.log(fileContents);
+    //     } catch (err) {
+    //       console.log('error is: ', err);
+    //     }
+    //   }
+
+
+    //Uuidv4 kodlarını coupon codes txt nin içine ekliyor.
     generate() {
         try {
             fs.appendFile('../json/coupon-codes.txt',`${uuidv4()}\n`, function(err:any) {
