186869936fc6ac7afa925e42c5d89b69009b00b9
CANMERTINYO <38213551+canmertinyo@users.noreply.github.com>
2022-12-31 18:31:03 +0300

    uuidv4 coupons

--- dist/services/generate-random-code.js
+++ dist/services/generate-random-code.js
@@ -2,9 +2,19 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.RandomCouponCode = void 0;
 const uuid_1 = require("uuid");
+const fs = require('fs');
 class RandomCouponCode {
     generate() {
-        return (0, uuid_1.v4)();
+        try {
+            fs.appendFile('../json/coupon-codes.json', `${(0, uuid_1.v4)()}\n`, function (err) {
+                if (err)
+                    throw err;
+                console.log("Kod oluşturuldu...");
+            });
+        }
+        catch (e) {
+            console.log("erdfdssdfr", e);
+        }
     }
 }
 exports.RandomCouponCode = RandomCouponCode;
--- services/generate-random-code.ts
+++ services/generate-random-code.ts
@@ -1,7 +1,18 @@
 import { v4 as uuidv4 } from 'uuid';
-
+const fs = require('fs')
 export class RandomCouponCode {
+
     generate() {
-        return uuidv4();
+        try {
+            fs.appendFile('../json/coupon-codes.json',`${uuidv4()}\n`, function(err:any) {
+                if(err) throw err;
+                console.log("Kod oluşturuldu...");
+            })
+        }
+        catch(e) {
+            console.log("erdfdssdfr",e)
+        }
+        
     }
+
 }
