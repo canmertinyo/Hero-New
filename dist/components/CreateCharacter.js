b09813a8fae0eb4d3f09ed26a8c6d2fac2f29b5d
Can Çevik <mazlumcancevik@gmail.com>
2023-01-04 20:30:58 +0300

    remove dist

--- dist/components/CreateCharacter.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.CreateCharacter = void 0;
-class CreateCharacter {
-    constructor(character) {
-        this.character = character;
-    }
-}
-exports.CreateCharacter = CreateCharacter;
