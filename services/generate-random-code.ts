0f75cce82e1c3abea3187c9495d1cad4648e46a2
CANMERTINYO <38213551+canmertinyo@users.noreply.github.com>
2023-01-06 16:36:31 +0300

    ..

--- services/generate-random-code.ts
+++ services/generate-random-code.ts
@@ -1,25 +1,14 @@
 import { v4 as uuidv4 } from 'uuid'
-import fs from 'fs'
 
 export class RandomCouponCode {
   private UUIDV4 = uuidv4()
 
-  public generate(): string {
+  public generate(): string | unknown {
     try {
-      fs.appendFile(__dirname + './../json/coupon-codes.txt', `${this.UUIDV4}\n`, (err) => {
-        if (err) throw err
-        console.log(`
-                                            Hello adventurer! Your coupon has been created!
-                                            ************************
-
-                                            Code : Open your coupen-codes.txt on your local computer. We send it through the in txt file.
-                                            Hint : To use your coupon code, simply just put your code in the text area. Then hit the submit button!
-                                            ************************
-                `)
-      })
+      console.log(this.UUIDV4)
       return this.UUIDV4
-    } catch (e) {
-      throw new Error('Something went wrong.')
+    } catch (error: unknown) {
+      return error
     }
   }
 }
