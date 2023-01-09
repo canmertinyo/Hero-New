81b4a6f7e9fc2c464013194133ea3a3eb5ad5692
CANMERTINYO <38213551+canmertinyo@users.noreply.github.com>
2023-01-09 23:08:36 +0300

    Add files via upload

---
+++ enums/exception-list.ts
@@ -0,0 +1,5 @@
+export enum ErrorList {
+    BLANK_USERNAME = 'Your username is cannot be empty!',
+    BLANK_PASSWORD = 'Your password is cannot be empty!',
+    DUPLICATED_CHARACTER = 'This username is already taken!'
+}
