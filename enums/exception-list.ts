c9cf370be73979005d88df1675ea9194cf459f37
CANMERTINYO <38213551+canmertinyo@users.noreply.github.com>
2023-01-10 01:04:30 +0300

    prettier optimization

--- enums/exception-list.ts
+++ enums/exception-list.ts
@@ -1,5 +1,5 @@
 export enum ErrorList {
-    BLANK_USERNAME = 'Your username is cannot be empty!',
-    BLANK_PASSWORD = 'Your password is cannot be empty!',
-    DUPLICATED_CHARACTER = 'This username is already taken!'
+  BLANK_USERNAME = 'Your username is cannot be empty!',
+  BLANK_PASSWORD = 'Your password is cannot be empty!',
+  DUPLICATED_CHARACTER = 'This username is already taken!'
 }
