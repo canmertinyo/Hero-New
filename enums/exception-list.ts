729a924ddb5220f420fc9dee91bfbed93c873bb2
canmertinyo <38213551+canmertinyo@users.noreply.github.com>
2023-01-10 11:59:20 +0300

    Delete exception-list.ts

--- enums/exception-list.ts
+++
@@ -1,5 +0,0 @@
-export enum ErrorList {
-  BLANK_USERNAME = 'Your username is cannot be empty!',
-  BLANK_PASSWORD = 'Your password is cannot be empty!',
-  DUPLICATED_CHARACTER = 'This username is already taken!'
-}
